b99228b32f11c2c1e26fec9bd1ea0e65e2e0e471
Ilya Kolo <ilya.kolo@gmail.com>
2014-10-02 18:12:00 +0400

    Auto install brew on Mac and compile YCM

--- run.sh
+++ run.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 
+set -e
+
+cd `dirname $0`
+
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/Vundle.vim
 
 cp .vimrc ~/
 vim +PluginInstall +qall
+
+sw_vers -productVersion 2>/dev/null 1>&2
+
+if [ $? -eq 0 ]
+then
+	which brew 2>/dev/null 1>&2
+
+	if [ $? -ne 0 ]
+	then
+		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	fi
+fi
+
+cd ~/.vim/bundle/YouCompleteMe
+./install.sh
